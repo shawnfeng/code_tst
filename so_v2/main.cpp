f07109c97e92fda9927f77a6c21ee18ca5f29f05
Shawn <fengguangxiang@gmail.com>
2013-09-11 14:28:55 +0800

    so_v

---
+++ so_v2/main.cpp
@@ -0,0 +1,12 @@
+#include "hello.h"
+
+void main_hello()
+{
+  //  hello();
+}
+
+int main()
+{
+  main_hello();
+  return 0;
+}
---
+++ so_v_new_test/hello.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "hello.h"
+void in_hello();
+
+void hello()
+{
+	printf("%s\n", "Hello");
+	//in_hello();
+}
+
