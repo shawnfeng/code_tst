8e870fc36641f9571e4e4f24e0dfbb26939ebb19
AutoMerge <fengguangxiang@edaijia-staff.cn>
2014-07-30 16:37:11 +0800

    push_server package

--- mygo/src/push_server/tcpconn/tcpconn.go
+++ mygo/src/push_server/tcpconn/tcpconn.go
@@ -1,4 +1,4 @@
-package main
+package tcpconn
 
 // TODO LIST:
 // select add timeout, expecial Client.Send
@@ -231,33 +231,3 @@ func (self *ConnectionManager) Trans() {
 
 }
 
-func main() {
-	var conn_man ConnectionManager
-	conn_man.Init()
-	go conn_man.Req()
-	go conn_man.Trans()
-
-	service := ":9988"
-	tcpAddr, error := net.ResolveTCPAddr("tcp", service)
-	if error != nil {
-		log.Println("Error: Could not resolve address")
-	} else {
-		netListen, error := net.Listen(tcpAddr.Network(), tcpAddr.String())
-		if error != nil {
-			log.Println(error)
-		} else {
-			defer netListen.Close()
-
-			for {
-				log.Println("Waiting for clients")
-				connection, error := netListen.Accept()
-				if error != nil {
-					log.Println("Client error: ", error)
-				} else {
-					conn_man.AddClient(connection)
-
-				}
-			}
-		}
-	}
-}
