e58cc762032b569c05e3433290c2225c3c4713a5
AutoMerge <fengguangxiang@edaijia-staff.cn>
2014-07-30 16:22:47 +0800

    rediscluster

--- mygo/src/rediscluster/redix.go
+++ mygo/src/rediscluster/redix.go
@@ -1,6 +1,6 @@
 // example program
 
-package main
+package rediscluster
 
 import (
 //	"fmt"
@@ -87,29 +87,5 @@ func NewRedisPool() *RedisPool {
 
 //////////
 
-
-func main() {
-	pool := NewRedisPool()
-	cmd0 := []interface{}{"echo", "Hello world A0!"}
-	cmd1 := []interface{}{"echo", "Hello world B0!"}
-	cmd2 := []interface{}{"echo", "Hello world C0!"}
-
-	//log.Println(reflect.TypeOf(cmd0[0]))
-
-	mcmd := make(map[string][]interface{})
-	mcmd["127.0.0.1:9600"] = cmd0
-	mcmd["127.0.0.1:9601"] = cmd1
-	mcmd["127.0.0.1:9602"] = cmd2
-
-	log.Println(mcmd)
-	rp := pool.Cmd(mcmd)
-
-	log.Println(rp)
-
-
-
-
-
-}
 
 // 支持apply, 一次多个命令过去,并统一获得返回
